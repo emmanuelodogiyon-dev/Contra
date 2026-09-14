abda37f358558c7ec74fcbabf910026715f9fa2b
emmanuelodogiyon-dev <emmanuelodogiyon@gmail.com>
2026-09-14 17:24:38 +0100

    Fix Social Bar loading

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
       <head />
       <body>
         {children}
-        <script src="https://pl31341524.profitableratecpmnetwork.com/be/07/72/be077243b749930a02e277af96dcaa5b.js"></script>
+        <script data-cfasync="false" src="https://pl31341524.profitableratecpmnetwork.com/be/07/72/be077243b749930a02e277af96dcaa5b.js"></script>
       </body>
     </html>
   )
