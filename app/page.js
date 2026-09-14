8f6123bd961fc84dbe4ec08f7a5034fee2db9c9b
emmanuelodogiyon-dev <emmanuelodogiyon@gmail.com>
2026-09-14 17:07:33 +0100

    Place native banner on Shadow Strike page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import AuthGate from '../components/AuthGate'
 import InterstitialAds from '../components/InterstitialAds'
+import NativeBanner from '../components/NativeBanner'
 
 export default function Home() {
-  return <main><InterstitialAds /><AuthGate /></main>
+  return <main><NativeBanner /><InterstitialAds /><AuthGate /></main>
 }
